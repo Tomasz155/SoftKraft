0a4cefe335fd4dbe45c3476090dfd286aaa37911
TomaszCa <t.calka@mdtrading.pl>
2021-03-16 21:12:57 +0000

    add filter as new function

--- app/Http/Controllers/categoriesProductController.php
+++ app/Http/Controllers/categoriesProductController.php
@@ -7,27 +7,33 @@ use DB;
 
 class categoriesProductController extends Controller
 {
-    
+
     public function index($categorySlug, Request $request)
     {
-        $filter = $request->filter;
+        $filterArray = $this->filter($request);
 
+        return DB::select('select p.* from products p '.$filterArray['join_type'].' join order_items i on i.product_id=p.id inner join product_categories pc on pc.product_id=p.id inner join categories c on c.id=pc.category_id where c.slug=? '.$filterArray['sql_condition'], [$categorySlug]);
+
+    }
+
+    public function filter($request)
+    {
+        $filter = $request->filter;
         switch ($filter){
             case 'without_order':
                 $join_type = 'left';
-                $sql = 'and i.id is null';
+                $sql_condition = 'and i.id is null';
                 break;
             case 'with_order':
                 $join_type = 'inner';
-                $sql = '';
+                $sql_condition = '';
                 break;
             case 'all':
                 $join_type = 'left';
-                $sql = '';
+                $sql_condition = '';
                 break;
         }
-
-        return DB::select('select p.* from products p '.$join_type.' join order_items i on i.product_id=p.id inner join product_categories pc on pc.product_id=p.id inner join categories c on c.id=pc.category_id where c.slug=? '.$sql, [$categorySlug]);    
-
+        return ['join_type' => $join_type, 'sql_condition' => $sql_condition];
     }
+
 }
